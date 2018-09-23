5ce08ca529497e8abb0d197dfba5df40abd1fee1
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:46:52 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan12_Aritmatika.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan12_aritmatika;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan12_Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
